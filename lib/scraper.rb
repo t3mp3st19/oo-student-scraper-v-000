ca961f25dba6717295e3669b2ccfae1d58b6275e
Chrissa Constantine <chrissa1@mac.com>
2018-05-31 20:24:14 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
     students.css(".student-card").text
 		name = students.css(".student-name").text
 		location = students.css(".student-location").text
-		url = students.css("href").text
+		url = students.css("div.student-card a").text
     #student
   end
 

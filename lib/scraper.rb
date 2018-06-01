293dad7a3702db1a400172ea0db6fc27df401117
Chrissa Constantine <chrissa1@mac.com>
2018-06-01 00:37:26 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,14 +26,10 @@ class Scraper
       details = doc.css(".vitals-container")
       links_array = details.css(".social-icon-container a")
       links_array.each do |link|
-        if link.attributes["href"].value.include?("twitter")
-          scraped_student[:twitter] = link.attributes["href"].value
-        elsif link.attributes["href"].value.include?("linkedin")
-          scraped_student[:linkedin] = link.attributes["href"].value
-        elsif link.attributes["href"].value.include?("github")
-          scraped_student[:github] = link.attributes["href"].value
-        else
-          scraped_student[:blog] = link.attributes["href"].value
+        scraped_student[:github] = link["href"] if /github/.match(link["href"])
+        scraped_student[:twitter] = link["href"] if /twitter/.match(link["href"])
+        scraped_student[:linkedin] = link["href"] if /linkedin/.match(link["href"])
+        scraped_student[:blog] = link["href"] if !/(github|linkedin|twitter)/.match(link["href"])
         end
       end
       scraped_student[:profile_quote] = details.css(".profile-quote").text

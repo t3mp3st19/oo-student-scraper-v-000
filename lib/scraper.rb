2f5790a179c04a9ca8770463957bace05628fcd4
Chrissa Constantine <chrissa1@mac.com>
2018-06-01 00:30:14 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,11 +26,16 @@ class Scraper
       details = doc.css(".vitals-container")
       links_array = details.css(".social-icon-container a")
       links_array.each do |link|
-        scraped_student[:github] = link["a"] if /github/.match(link['href'])
-      scraped_student[:twitter] = link["a"] if /twitter/.match(link['href'])
-      scraped_student[:linkedin] = link["a"] if /linkedin/.match(link['href'])
-      scraped_student[:blog] = link["a"] if !/(github|linkedin|twitter)/.match(link['href'])
-    end
+        if link.attributes["href"].value.include?("twitter")
+          scraped_student[:twitter] = link.attributes["href"].value
+        elsif link.attributes["href"].value.include?("linkedin")
+          scraped_student[:linkedin] = link.attributes["href"].value
+        elsif link.attributes["href"].value.include?("github")
+          scraped_student[:github] = link.attributes["href"].value
+        else
+          scraped_student[:blog] = link.attributes["href"].value
+        end
+      end
       scraped_student[:profile_quote] = details.css(".profile-quote").text
       scraped_student[:bio] = doc.css(".details-container .bio-block .bio-content .description-holder p").text
       scraped_student
